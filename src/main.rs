c861f645ecba3abbc7b3e11e6d1c15bf276b08bb
Aarys Sahler <aarys.sahler@gmail.com>
2025-01-12 08:55:01 -0800

    finally WORKS :DDDD

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@
 use std::panic::resume_unwind;
 use rocket::futures::{SinkExt, StreamExt};
 use futures::future::join_all;
+use futures::lock::Mutex;
 
 use rocket_db_pools::{Connection, Database, deadpool_redis};
 use rocket_db_pools::deadpool_redis::redis::AsyncCommands;
 use ws::stream::DuplexStream;
 
-use std::sync::{Mutex, Arc};
+use std::sync::Arc;
 use futures::stream::SplitSink;
 use ws::Message;
 
@@ -19,7 +20,6 @@ pub struct RedisPool(deadpool_redis::Pool);
 struct Channels {
     channels: Arc<Mutex<Vec<Arc<Mutex<SplitSink<DuplexStream, Message>>>>>>
 }
-
 
 
 #[get("/")]
@@ -41,70 +41,33 @@ async fn get_message(id: isize, mut conn: Connection<RedisPool>) -> String {
 async fn push_new_message(message: &str, mut conn: Connection<RedisPool>, channels_state: &rocket::State<Channels>) -> String {
     println!("New message: {}", message);
 
-    let ws_msg = Message::text("olaaaa");
-    let mut send_futures = Vec::new();
-
-    // First, collect all channel references while holding the outer lock
-    let channel_refs = {
-        let channels = channels_state.channels.lock().unwrap();
-        channels.iter().cloned().collect::<Vec<_>>()
-    };
-
-    // Create futures for each send operation
-    for channel in channel_refs {
-        let msg = ws_msg.clone();
-        send_futures.push(async move {
-            // Take a new lock for each send operation
-            let message_result = {
-                if let Ok(mut stream) = channel.lock() {
-                    stream.send(msg)
-                } else {
-                    return println!("Failed to acquire lock for channel");
-                }
-            };  // Lock is dropped here
-
-            // Now await the send operation without holding the lock
-            if let Err(e) = message_result.await {
-                println!("Failed to send message: {:?}", e);
-            }
-        });
-    }
-
-    // Execute all send operations concurrently
-    join_all(send_futures).await;
-
-    match conn.rpush::<&str, Vec<&str>, i32>("messages", vec![message]).await {
+    let new_record = match conn.rpush::<&str, Vec<&str>, i32>("messages", vec![message]).await {
         Ok(id) => (id - 1).to_string(),
         Err(_) => "An error occurred".to_string()
+    };
+
+    match new_record {
+        Ok(id) => {
+            // Broadcast new message to all channels
+            let ws_msg = Message::text(format!("N{}", id));
+            let channels_lock = channels_state.channels.try_lock().unwrap();
+
+            for channel in channels_lock.iter() {
+                let mut channel_clone = channel.clone();
+                let mut channel_clone_lock = channel_clone.lock().await;
+
+                let _ = channel_clone_lock.send(ws_msg.clone()).await.unwrap();
+            }
+
+            id.to_string()
+        }
+        Err(err) => err
     }
 }
 
 
 #[get("/echo?channel")]
 async fn echo_channel<'a>(ws: ws::WebSocket, channels_state: &'a rocket::State<Channels>) -> ws::Channel<'a> {
-    /*ws.channel(move |mut stream| Box::pin(async move {
-        let boxed = Box::new(stream);
-
-        let channels_inner = channels.inner();
-        channels_inner.channels.lock().unwrap().push(boxed);
-
-
-        /* while let Some(message) = stream.next().await {
-            let _ = stream.send(message?).await;
-        } */
-
-        Ok(())
-    }))*/
-
-    /*ws.channel(move |stream| Box::pin(async move {
-        let boxed = Box::new(stream);
-
-        let channels_inner = channels.inner();
-        channels_inner.channels.lock().unwrap().push(boxed);
-
-        Ok(())
-    }))*/
-
     ws.channel(move |stream| Box::pin(async move {
         let (sender, _) = stream.split();
         let arc_sender = Arc::new(Mutex::new(sender));
